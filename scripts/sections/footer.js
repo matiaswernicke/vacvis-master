ba5684b631cc8cc11bb2b03d69f5892a3d514fd5
Matias Wernicke <mwernicke@swci.com.ar>
2019-01-28 19:44:05 -0300

    20190128_1943
    Nro de vercion

--- scripts/sections/footer.js
+++ scripts/sections/footer.js
@@ -1,7 +1,7 @@
 var TEMPLATE_FOOTER = ''
 +'<footer class="footer-wrapper" role="contentinfo">'
 +'  <div class="footer">'
-+'    <div class="pull-right text-muted"><small>v 1.0.5</small></div>'
++'    <div class="pull-right text-muted"><small>v 2.1.0</small></div>'
 +'    <div>Vacunar  &copy; 2018</div>'
 +'  </div>'
 +'</footer><!-- /.FOOTER -->';
